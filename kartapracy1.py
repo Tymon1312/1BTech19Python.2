4b66b53e6f8ae65c888f6fc33709a0fa036ca474
1B28OT <114583431+Tymon1312@users.noreply.github.com>
2022-10-05 20:15:04 +0000

    wszystkie zrobione zadania

--- kartapracy1.py
+++ kartapracy1.py
@@ -1,40 +1,40 @@
 # zad 1
 
-# a = int(input())
-# b = int(input())
-# print(a*a + b*b)
+a = int(input())
+b = int(input())
+print(a*a + b*b)
 
 # zad 2
 
-# a = int(input())
-# b = int(input())
-# print((a=b**2)
+a = int(input())
+b = int(input())
+print ((a + b)**2)
 
 # zad 3
 
-# a = int(input())
-# b = int(input())
-# print ((a-b)**3 )
+a = int(input())
+b = int(input())
+print ((a-b)**3 )
 
 # zad 4
 
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# print ((a*b*c))
+a = int(input())
+b = int(input())
+c = int(input())
+print ((a*b*c))
 
 # zad 5
 
-# a = int(input())
-# b = int(input())
-# print (((a+b)*2)/5)
+a = int(input())
+b = int(input())
+print (((a+b)*2)/5)
 
 # zad 6
 
-# brutto = float(input())
-# print(brutto / 1.23)
+brutto = float(input())
+print(brutto / 1.23)
 
 # zad 7
 
-# a, b = int(input()), int(input())
-# print ((a%b))
+a, b = int(input()), int(input())
+print ((a%b))
