7d4dd5efce8b44bf27ace0f1bb299682131a4c30
1B28OT <114583431+Tymon1312@users.noreply.github.com>
2022-10-12 12:15:00 +0000

    zrobione wszystkie zadania

--- kartapracy2.py
+++ kartapracy2.py
@@ -1,26 +1,49 @@
-# #zad 1
+#zad 1
 # n = int(input())
 # if n % 3 == 0:
 #   print("TAK")
 # else:
 #   print("NIE")
 
-# #zad 2
+#zad 2
 # n = int(input())
 # if n >= 100 and n <1000 and n % 17 == 0:
 #   print("TAK")
 # else:
 #   print("NIE")
-#   #zad 3 
+#zad 3 
 # n = int(input())
 # if n >= 18:
 #   print ("TAK")
 # else:
 #   print ("NIE")
-  #zad 4
-limit = 20
-waga = int(input())
-if waga <=limit:
-  print("JAZDA")
-else:
-  print("trocge za ciężko")
+#zad 4
+# limit = 20
+# waga = int(input())
+# if waga <=limit:
+#   print("JAZDA")
+# else:
+#   print("trocge za ciężko")
+#zad 5
+# a= int(input())
+# b= int(input())
+# c= int(input())
+# if c > a and c < b or c > b and c < a:
+#  print ("TAK")
+# else:
+#  print ("NIE")
+  #zad 6
+# a= int(input())
+# b= int(input())
+# if (a**p-a) % p == 0:
+# print("TAK, spełnia")
+# else:
+# print("NIE, nie spełnia")
+#zad 7
+# p= int(input())
+# k= int(input())
+# s= int(input())
+# if s*3>=k-p:
+#   print("TAK")
+# else:
+#   print("NIE")
