f4fb85032607036bfd22d41e7fca7ce40dd2911f
1B28OT <114583431+Tymon1312@users.noreply.github.com>
2022-10-16 18:26:33 +0000

    wszystkie zadania zrobione

--- kartapracy2a.py
+++ kartapracy2a.py
@@ -17,11 +17,15 @@
 # l = int(input())
 # m = int(input())
 # if (k==l) and k!=m:
-#  print("k jest równe l")
-# if (l==m) and l!=k:
-#  print("l jest równe m")
-# if (m==k) and m!=l:
-#  print("k jest równe m")
+#   print("k jest równe l")
+# else:
+#  if (l==m) and l!=k:
+#   print("l jest równe m")
+#  else:
+#   if (m==k) and m!=l:
+#    print("k jest równe m")
+#   else:
+#    print("nie ma żadnych liczb takich samych")
 # zad4
 # a = int(input())
 # b = int(input())
@@ -29,12 +33,15 @@
 # d = int(input())
 # if a<b and a<c and a<d:
 #  print("a jest najmniejsze")
-# if b<a and b<c and b<d:
-#  print("b jest najmniejsze")
-# if c<a and c<b and c<d:
-#  print("c jest najmniejsze")
-# if d<a and d<b and d<c:
-#  print("d jest najmnijesze")
+# else:
+#  if b<a and b<c and b<d:
+#   print("b jest najmniejsze")
+#  else:
+#   if c<a and c<b and c<d:
+#    print("c jest najmniejsze")
+#   else:
+#    if d<a and d<b and d<c:
+#     print("d jest najmnijesze")
 # zad5
 # a = int(input())
 # b = int(input())
@@ -44,3 +51,14 @@
 # else:
 #   print("NIE")
 #zad6
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if (a**2+b**2==c**2) or (b**2+c**2==a**2) or (a**2+c**2==b**2):
+#   print("prostokątny")
+# else:
+#  if (a**2+b**2<c**2) or (b**2+c**2<a**2) or (a**2+c**2<b**2):
+#   print("rozwartokątny")
+#  else:
+#   if (a**2+b**2>c**2) or (b**2+c**2>a**2) or (a**2+c**2>c**2):
+#    print("ostrokątny")
