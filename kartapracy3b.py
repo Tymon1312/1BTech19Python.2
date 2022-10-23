18052644e1ab2981020b95200f2f6f588925c10a
1B28OT <114583431+Tymon1312@users.noreply.github.com>
2022-10-23 20:47:47 +0000

    zad 4 zrobione

---
+++ kartapracy3b.py
@@ -0,0 +1,16 @@
+#zad 1
+# for i in range(1,31):
+#   print(i, end=" ")
+#zad 2
+# for i in range(1,10,2):
+#   print(i**2, end=" ")
+#zad 3
+# for i in range(1000,10000):
+#  if i % 379==0:
+#   print(i, end=" ")
+#zad 4
+# for i in range(100,1000):
+#  if i % 5 == 0 and i % 6 ==0 or i % 11==0:
+#   print(i, end=" ")
+#zad 5
+
