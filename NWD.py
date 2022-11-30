b06e3b4428fd36408acf5bbbe86342dfb3bfa652
1B28OT <114583431+Tymon1312@users.noreply.github.com>
2022-11-30 13:14:10 +0000

    zrobiłem 3 zadania związane z NWW i NWD

---
+++ NWD.py
@@ -0,0 +1,25 @@
+# a = int(input())
+# b = int(input())
+
+# while a != b:
+#     if a > b : a = a - b 
+#     if b > a : b = b - a
+# print(a)
+#euklides modulo
+# a = int(input())
+# b = int(input())
+
+# while b > 0:
+#     a, b = b, a%b
+# print(a)
+
+# a = int(input())
+# b = int(input())
+# c = a * b
+# while a != b:
+#    if a > b : a = a - b
+#    if b > a : b = b - a
+#    print(a)
+#    break
+# d = c // a
+# print(d)
--- pierwsze.py
+++ pierwsze.py
@@ -1,8 +1,52 @@
 #sprawdzianie czy liczba jest pierwsza
 
-n = int(input())
-for i in range(2,n):
-  if n % i == 0:
-     print('Liczba nie jest pierwsza')
-     exit(0)
-print("liczba jest pierwsza")
+# n = int(input())
+# for i in range(2,n):
+#   if n % i == 0:
+#      print('Liczba nie jest pierwsza')
+#      exit(0)
+# print("liczba jest pierwsza")
+
+#Generowanie liczb pierwszych
+
+# n = int(input("Podaj do ilu mam szukać liczb pierwszych"))
+
+# for i in range(2, n+1):
+#     flaga = True;
+#     for j in range(2, i):
+#         if i % j == 0:
+#            flaga = False
+#            break
+#     if flaga:
+#        print(i, end=" ")
+
+
+#drugi sposób
+# n = int(input("Podaj do ilu mam szukać liczb pierwszych"))
+
+# for i in range(2, n+1):
+#     flaga = True;
+#     for j in range(2,int(i**0.5)+1):
+#         if i % j == 0:
+#            flaga = False
+#            break
+#     if flaga:
+#        print(i, end=" ")
+#3 generowanie liczb pierwszych (pierwsze n liczb pierwszych)
+n =int(input())
+i= 2
+licznik= 0
+while 1:
+    flaga = True;
+    for j in range(2,int(i**0.5)+1):
+         if i % j == 0:
+            flaga = False
+            break
+    if flaga == True:
+     print(i, end=" ")
+     licznik += 1
+    if licznik == p:
+         break
+    else:
+       i = i+1
+      print(i, end=" ")
